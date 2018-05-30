84adf82d69241a6c7d7ed718ea9967cea80f521e
Eduardo Alcantara de Oliveira~ <eoalcantara@gmail.com>
2018-05-30 14:40:50 -0300

    Google

---
+++ MvcAuth/MvcAuth/Startup.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MvcAuth.Data;
+using MvcAuth.Models;
+using MvcAuth.Services;
+
+namespace MvcAuth
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+
+            services.AddAuthentication().AddLinkedIn(options =>
+            {
+                options.ClientId = Configuration["Authentication:LinkedIn:ClientId"];
+                options.ClientSecret = Configuration["Authentication:LinkedIn:ClientSecret"];    
+            });
+
+            services.AddAuthentication().AddGoogle(options =>
+            {
+                options.ClientId = Configuration["Authentication:Google:ClientId"];
+                options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+            });
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+
+            //var google = new Microsoft.AspNetCore.Authentication.Google.GoogleOptions { ClientId = "740735999597-jnejf7vaf2frkhev4u2fqakbij1ms0d9.apps.googleusercontent.com", ClientSecret = "iiLujKZKsBg4xjhhrJL0upjt" };
+
+            //app.UseGoogleAuthentication(google);
+
+
+        }
+
+
+    }
+}
